b08f9888fee66f84235559276e10d29619699225
俊南 蔡 <442139759@qq.com>
2020-07-26 21:35:40 +0800

    an example to use it

---
+++ Example/Example.cpp
@@ -0,0 +1,18 @@
+﻿#include "Log.h"
+
+int main()
+{
+	Log::init();
+	Log::setConsoleLogLevel(Log::Level::Debug);
+	Log::setFileLogLevel(Log::Level::Trace);
+
+	Log::error("this is error");
+	Log::warn("this is warn");
+	Log::debug("this is debug");
+	Log::info("this is info");
+	Log::trace("this is trace");
+
+	Log::info("make a combo of {} and {}", 1, "word");
+
+	return 0;
+}
